a765bd16d064b7c2d02d73e32782a0eab8082f7c
AdoniasHaile <adoniashaile1@gmail.com>
2024-11-21 10:22:03 +0300

    Test database availablity

--- app/app/calc.py
+++
@@ -1,7 +0,0 @@
-""""
-calculator functions
-"""
-
-def add(x, y):
-    """Add x and y and return result"""
-    return x + y
--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -77,12 +78,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),  # Should be 'db'
+        'NAME': os.environ.get('DB_NAME'),  # Should be 'postgres'
+        'USER': os.environ.get('DB_USER'),  # Should be 'postgres'
+        'PASSWORD': os.environ.get('DB_PASSWORD'),  # Should be 'postgres'
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
