7d8c63ee63f1cc8418a6a70d771e04f1bb77f704
AdoniasHaile <adoniashaile1@gmail.com>
2024-11-24 18:21:19 +0300

    Add user update
    -Test user update

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,8 @@
 """
 View for the user API
 """
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -19,3 +21,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new token for  a user"""
     serializer_class = AuthTokenSerializer
     renderer_class = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
